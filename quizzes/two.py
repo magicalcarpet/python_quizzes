f22396303b2d0b3e9e2cf7e6b0ed4d4c72bc8353
Ade Oke <oke.ade@gmail.com>
2020-10-25 19:31:07 +0000

    added football data file

---
+++ quizzes/two.py
@@ -0,0 +1,11 @@
+# we are going to read the scores.dat file and grab all the teams within
+# we want to open scores.dat file
+# we want to regex and grab all the teams and then store them in a list
+
+
+# The file football.dat contains the results from the English Premier League for 2001/2. 
+# \The columns labeled ‘F’ and ‘A’ contain the total number of goals scored for and against 
+# each team in that season (so Arsenal scored 79 goals against opponents, and had 36 goals 
+# scored against them). Write a program to print the name of the team with the smallest 
+# difference in ‘for’ and ‘against’ goals.
+
